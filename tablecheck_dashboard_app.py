af58c9777abc14290b967e56a3b39539eed68014
noncreature0714 <noncreature0714@gmail.com>
2025-11-23 17:52:07 -0800

    quick capture commit so chaos does not strike and win

--- tablecheck_dashboard_app.py
+++ tablecheck_dashboard_app.py
@@ -207,3 +207,51 @@ st.dataframe(
 
 ''
 ''
+
+''' 
+How would you build this differently if the data was being streamed from Kafka?
+
+I'm answering this assuming I can imagine whatever I wish given this new context, to wit:
+    1. A production environment, with clear separation between streams, data lake, ETL pipelines, data warehouse, 
+       and dashboards/reporting
+    2. Separately and centrally managed configuration for each major project and/or devision (whichever makes sense)
+    3. Storage policies and other governance 
+
+Firstly, a stream reader which pulls from the Kafka stream for specified topics into a an hourly partitioned 
+data lake with an approxiamate 30 day retention policy. 
+
+Next, depending on the cadence, either hourly or daily (daily recommended), run one or several ETLs which aggregate and send
+data to a warehouse or "lake house," ready for consumption by dashboards or other teams within the company.
+
+
+
+
+'''
+
+''
+''
+
+''' 
+How would you improve the deployment of this system? 
+
+Assuming this is narrowly related to my current implementation using Streamlit and _ database, and not some imaginary 
+system, I would:
+
+    1. Make the code more testable and maintainable by extracting the logic into functions or objects
+        a. debatably the initial database connection could be managed with a context manager or a class
+            i. really depends on the size of the project and how much it needs future proofed without being
+               overly paranoid or wasting too much time (not that either would be a big time issue)
+        b. most of the logic here is organized by question, this was a tradeoff I made because it would 
+           be better to organize this into steps that iterate on the dataframe for groupings and aggregations
+           for each question, however, this was the tradeoof for using streamlit as well, and not some backend 
+           pipeline where the aggregate logic can be separated more discretely from the display logic
+    2. Add unit testing to each extract and transform 
+    3. Update pre-commit hooks to run tests and check for "success" before pushing
+    4. Upon succeful completion of the work, have a deployment (probably a mix of manual and some self-automation)
+       that tags the job in git, and pushes to the main branch upon a success pull/merge request. 
+    
+Importantly, I'd also have a local docker container (or similar) where I can inspect the results before pushing to the site
+on my local 
+
+
+'''
